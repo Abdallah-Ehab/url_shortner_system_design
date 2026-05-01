f3ff82ef7d09e6dd1c46a837edd2e4dffce865f5
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-01 23:30:08 +0300

    add encodeBase62 function for Base62 encoding of numbers

---
+++ services/encode.service.js
@@ -0,0 +1,14 @@
+export function encodeBase62(num) {
+  if (num === 0n) return CHARSET[0]
+
+  let result = ''
+  let n = num
+
+  while (n > 0n) {
+    const remainder = n % 62n
+    result = CHARSET[Number(remainder)] + result
+    n = n / 62n
+  }
+
+  return result
+}
