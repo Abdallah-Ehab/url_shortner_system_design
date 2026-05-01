fe5207ea25d392a20ad6ae05b51ac8973c77100d
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-01 23:59:48 +0300

    add IDProvider class for managing unique ID generation with Redis

---
+++ services/id_provider.service.js
@@ -0,0 +1,22 @@
+export class IDProvider {
+    constructor(redisClient, rangeSize = 1000n) {
+        this.redis = redisClient;
+        this.rangeSize = rangeSize;
+        this.currentID = 0n;
+        this.maxID = 0n;
+    }
+
+    async getNextID() {
+        if (this.currentID >= this.maxID) {
+            await this.refreshRange();
+        }
+        return ++this.currentID;
+    }
+
+    async refreshRange() {
+        // Atomic range claim from Redis
+        const newUpperBound = await this.redis.incrBy('global_id_counter', Number(this.rangeSize));
+        this.maxID = BigInt(newUpperBound);
+        this.currentID = this.maxID - this.rangeSize;
+    }
+}
