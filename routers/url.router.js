96b060ba7b3cb88e34f3bfc1390ad86611e4a67a
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-02 00:18:17 +0300

    refactor url.router.js to integrate IDProvider and UrlController for URL shortening and redirection

--- routers/url.router.js
+++ routers/url.router.js
@@ -1,13 +1,15 @@
 import router from 'express'
 import UrlController from '../controllers/url.controller.js'
+import { IDProvider } from '../services/id_provider.service.js'
+import redisClient from '../redis_client.js'
 
 const urlRouter = router()
 
 
-const urlController = new UrlController();
+const idProvider = new IDProvider(redisClient);
+const urlController = new UrlController(idProvider);
 
-urlRouter.post('/shorten', (req,res)=>{})
-urlRouter.get('/:shortUrl', (req,res)=>{})
-urlRouter.get('/analytics/:shortUrl', (req,res)=>{})
+urlRouter.post('/shorten', urlController.createShortUrl)
+urlRouter.get('/:shortUrl', urlController.redirectToOriginalUrl)
 
 export default urlRouter
