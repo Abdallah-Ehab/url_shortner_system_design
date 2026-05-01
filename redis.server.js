d68e02cb68a67469095a722f100d9aa20103372d
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-02 00:16:42 +0300

    remove unused express and dotenv imports from redis.server.js

--- redis.server.js
+++ redis.server.js
@@ -1,10 +1,4 @@
-import express from 'express';
 import redis from 'redis';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const app = express();
-const port = process.env.CACHE_PORT || 3003; // Port for the Redis server
 
 // Initialize Redis client (defaults to 127.0.0.1:6379)
 const client = redis.createClient();
@@ -14,3 +8,5 @@ client.on('error', (err) => console.log('Redis Client Error', err));
 (async () => {
     await client.connect(); // Connect to Redis
 })();
+
+export default client;
