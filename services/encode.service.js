67bb3b19b30b65552e53ca105d4a2cb7275632e0
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-06 02:01:30 +0300

    refactor(encode.service.js): convert encodeBase62 function to method of EncoderService class

--- services/encode.service.js
+++ services/encode.service.js
@@ -1,16 +1,18 @@
-const CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export default class EncoderService {
 
-export function encodeBase62(num) {
-  if (num === 0n) return CHARSET[0]
+  encode(num) {
+        const CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (num === 0n) return CHARSET[0]
 
-  let result = ''
-  let n = num
+        let result = ''
+        let n = num
 
-  while (n > 0n) {
-    const remainder = n % 62n
-    result = CHARSET[Number(remainder)] + result
-    n = n / 62n
-  }
+        while (n > 0n) {
+            const remainder = n % 62n
+            result = CHARSET[Number(remainder)] + result
+            n = n / 62n
+        }
 
-  return result
+        return result
+    }
 }
