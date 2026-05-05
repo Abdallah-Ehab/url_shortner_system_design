0c444e6fa9629226021b7d9da6761086bdcf3608
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-06 01:41:14 +0300

    fix(shorten.server.js): correct import path for Redis client and initialize URL router

--- shorten.server.js
+++ shorten.server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import urlRouter from './routers/url.router.js'
 import db_connection from './db_connection.js'
-import redisClient from './redis_client.js'
+import redisClient from './redis.client.js'
+import { IDProvider } from './services/id_provider.service.js'
+import createUrlRouter from './routers/url.router.js'
+
+
 dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use('/api/url', urlRouter)
 
 app.get('/api/healthCheck', async (req, res) => {
   try{
@@ -25,6 +28,12 @@ const startServer = async () => {
   try{
     await redisClient.connect()
     console.log('Connected to Redis')
+
+    const idProvider = IDProvider.getInstance(redisClient, 1000n);
+
+    const urlRouter = createUrlRouter(idProvider);
+
+    app.use('/api/url', urlRouter)
 
     app.listen(process.env.shorten, (req, res) => {
       console.log(`Server is running on port ${process.env.shorten}`)
