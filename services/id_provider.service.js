bdff6474b7265e1bf3b5941e7bbc8abdb89254c1
AbdallahEhab <abdallahehab3710@gmail.com>
2026-05-06 02:01:36 +0300

    refactor(id_provider.service.js): rename refreshRange method to _refreshRange for consistency

--- services/id_provider.service.js
+++ services/id_provider.service.js
@@ -19,13 +19,12 @@ export class IDProvider {
 
     async getNextID() {
         if (this.currentID >= this.maxID) {
-            await this.refreshRange();
+            await this._refreshRange();
         }
         return ++this.currentID;
     }
 
-    async refreshRange() {
-        // Atomic range claim from Redis
+    async _refreshRange() {
         const newUpperBound = await this.redis.incrBy('global_id_counter', Number(this.rangeSize));
         this.maxID = BigInt(newUpperBound);
         this.currentID = this.maxID - this.rangeSize;
